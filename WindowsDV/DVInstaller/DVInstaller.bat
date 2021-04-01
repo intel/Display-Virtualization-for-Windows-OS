91fda9f1894155d2df90b06167baf11aed341417
Parida, Malaya Kumar <malaya.kumar.parida@intel.com>
2021-04-01 15:01:03 +0530

    Removed add Reg Key from DVInstaller.bat, which is taken care inside Gfx Driver

--- WindowsDV/DVInstaller/DVInstaller.bat
+++ WindowsDV/DVInstaller/DVInstaller.bat
@@ -5,9 +5,6 @@ REM -------------------------------------
 
 @echo off
 echo Start driver installations...
-reg add "HKLM\SOFTWARE\Intel\KMD\Guc" /v GuCEnableNewInterface /t REG_DWORD /d 0x1 /f
-reg add "HKLM\SOFTWARE\Intel\KMD\SRIOV" /v VFSkipRelayService /t REG_DWORD /d 0x1 /f 
-reg add "HKLM\SOFTWARE\Intel\KMD\SRIOV" /v EnableFTRPAAC /t REG_DWORD /d 0x1 /f
 pnputil.exe /add-driver .\GraphicsDriver\iigd_dch.inf /install
 pnputil.exe /i /a .\IVSHMEM\ivshmem.inf
 devcon.exe install .\DVServer\DVServer.inf Root\DVServer
